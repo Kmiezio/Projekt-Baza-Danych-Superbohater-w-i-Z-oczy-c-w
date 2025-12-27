0e36f5d9f0ae39494bb1513d843597111391f47d
Kmiezio <107400240+Kmiezio@users.noreply.github.com>
2025-12-27 18:57:10 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,1 +1,6 @@
-
+//Plik odpowiada za interakcję z użytkownikiem.
+//Wyświetla menu tekstowe, pobiera wybory użytkownika 
+// i wywołuje odpowiednie funkcje logiki programu.
+int main(void) {
+    
+}
