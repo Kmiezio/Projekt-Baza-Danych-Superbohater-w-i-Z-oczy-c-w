2025ca6b41c95bfb797a42279c9ad17d8d84e028
Kmiezio <107400240+Kmiezio@users.noreply.github.com>
2025-12-27 18:58:55 +0100

    Update postac.h

--- postac.h
+++ postac.h
@@ -1,1 +1,5 @@
-
+//Definiuje strukturę danych opisującą pojedynczą postać oraz typy wyliczeniowe roli i statusu.
+//Stanowi wspólną definicję danych wykorzystywaną w całym programie.
+int main(void) {
+    
+}
