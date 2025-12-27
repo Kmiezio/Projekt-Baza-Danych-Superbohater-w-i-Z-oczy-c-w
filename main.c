8bc118ae6cc79ae80824ab23e821a3011a50a464
Kmiezio <107400240+Kmiezio@users.noreply.github.com>
2025-12-27 19:27:30 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,20 @@
+//Plik zawiera funkcję main, będącą punktem startowym programu.
+// Odpowiada za uruchomienie menu głównego oraz zakończenie programu.
+#include <stdio.h>
+#include "menu.h"
+
+int main(void) {
+    int wybor;
+
+    do {
+        wyswietl_menu();
+        wybor = pobierz_wybor();
+
+        printf("Wybrales opcje: %d\n", wybor);
+
+    } while (wybor != 0);
+
+    printf("Koniec programu.\n");
+    return 0;
+}
 
