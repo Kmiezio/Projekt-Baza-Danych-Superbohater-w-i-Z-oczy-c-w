c0b6a2227e43b67488dd836bff6f693cd314e480
Kmiezio <107400240+Kmiezio@users.noreply.github.com>
2025-12-30 00:17:29 +0100

    Update postac.h

--- postac.h
+++ postac.h
@@ -1,5 +1,33 @@
-//Definiuje strukturę danych opisującą pojedynczą postać oraz typy wyliczeniowe roli i statusu.
-//Stanowi wspólną definicję danych wykorzystywaną w całym programie.
-int main(void) {
-    
-}
+#ifndef POSTAC_H
+#define POSTAC_H
+
+#define MAX_TXT 100
+
+typedef enum {
+    BOHATER,
+    ZLOCZYNCA,
+    ANTYBOHATER,
+    NEUTRALNY
+} Rola;
+
+typedef enum {
+    AKTYWNY,
+    USPIONY,
+    W_MISJI,
+    RANNY,
+    UWIEZIONY
+} Status;
+
+typedef struct {
+    char pseudonim[MAX_TXT];
+    Rola rola;
+    char moc[MAX_TXT];
+    int poziom_zagrozenia;
+    char dzielnica[MAX_TXT];
+    Status status;
+} Postac;
+
+/* ✔️ TYLKO to */
+Postac utworz_postac(void);
+
+#endif
