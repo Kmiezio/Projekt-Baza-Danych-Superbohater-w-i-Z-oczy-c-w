4571fdf4ded1edda3742c2aa325cd260206092cb
Kmiezio <107400240+Kmiezio@users.noreply.github.com>
2025-12-30 22:33:28 +0100

    Update main.c

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 int main(void) {
     Lista lista;
     inicjalizuj_liste(&lista);
-
     int wybor;
+
     do {
         wyswietl_menu();
         wybor = pobierz_wybor();
@@ -15,12 +15,26 @@ int main(void) {
         switch (wybor) {
             case 1: {
                 Postac p = utworz_postac();
-                dodaj_postac(&lista, p); 
+                dodaj_postac(&lista, p);
                 break;
             }
+
+            case 5: {
+                wyswietl_liste(&lista);
+                break;
+            }
+
+            case 0:
+                printf("Zamykanie programu...\n");
+                break;
+
+            default:
+                printf("Niepoprawny wybor.\n");
+                break;
         }
 
     } while (wybor != 0);
 
+    zwolnij_liste(&lista);
     return 0;
 }
