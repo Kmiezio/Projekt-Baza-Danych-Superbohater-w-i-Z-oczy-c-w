d3ce682fbb5c5703774a9479c5cdf08b790f92c6
Kmiezio <107400240+Kmiezio@users.noreply.github.com>
2025-12-30 22:34:35 +0100

    Update lista.c

--- lista.c
+++ lista.c
@@ -1,5 +1,54 @@
-//Realizuje obsługę listy dynamicznej przechowującej postacie.
-//Zawiera funkcje dodawania, edycji, usuwania, wyszukiwania oraz wyświetlania danych.
-int main(void) {
-    
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+void inicjalizuj_liste(Lista *lista) {
+    lista->head = NULL;
+}
+
+void dodaj_postac(Lista *lista, Postac p) {
+    Wezel *nowy = malloc(sizeof(Wezel));
+    if (!nowy) return;
+
+    nowy->dane = p;
+    nowy->next = NULL;
+
+    if (lista->head == NULL) {
+        lista->head = nowy;
+        return;
+    }
+
+    Wezel *tmp = lista->head;
+    while (tmp->next != NULL) {
+        tmp = tmp->next;
+    }
+
+    tmp->next = nowy;
+}
+
+void wyswietl_liste(const Lista *lista) {
+    Wezel *tmp = lista->head;
+
+    if (tmp == NULL) {
+        printf("Lista jest pusta.\n");
+        return;
+    }
+
+    while (tmp != NULL) {
+        printf("Pseudonim: %s | Zagrozenie: %d | Dzielnica: %s\n",
+               tmp->dane.pseudonim,
+               tmp->dane.poziom_zagrozenia,
+               tmp->dane.dzielnica);
+        tmp = tmp->next;
+    }
+}
+
+void zwolnij_liste(Lista *lista) {
+    Wezel *tmp;
+
+    while (lista->head != NULL) {
+        tmp = lista->head;
+        lista->head = lista->head->next;
+        free(tmp);
+    }
 }
