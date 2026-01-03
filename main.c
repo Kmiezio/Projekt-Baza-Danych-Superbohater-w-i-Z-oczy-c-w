6e8fd20111ec73a091c18789b480455869b8da32
Kmiezio <107400240+Kmiezio@users.noreply.github.com>
2026-01-03 19:52:47 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "menu.h"
 #include "lista.h"
 #include "postac.h"
@@ -13,24 +14,52 @@ int main(void) {
         wybor = pobierz_wybor();
 
         switch (wybor) {
-            case 1: {
-                Postac p = utworz_postac();
-                dodaj_postac(&lista, p);
-                break;
+
+        case 1: {
+            Postac p = utworz_postac();
+            dodaj_postac(&lista, p);
+            break;
+        }
+
+        case 2: {
+            char pseudonim[MAX_TXT];
+            printf("Pseudonim do edycji: ");
+            fgets(pseudonim, MAX_TXT, stdin);
+            pseudonim[strcspn(pseudonim, "\n")] = 0;
+
+            Postac *p = znajdz_postac(&lista, pseudonim);
+            if (p) {
+                edytuj_postac(p);
+                printf("Postac edytowana.\n");
+            } else {
+                printf("Nie znaleziono postaci.\n");
             }
+            break;
+        }
 
-            case 5: {
-                wyswietl_liste(&lista);
-                break;
-            }
+        case 3: {
+            char pseudonim[MAX_TXT];
+            printf("Pseudonim do usuniecia: ");
+            fgets(pseudonim, MAX_TXT, stdin);
+            pseudonim[strcspn(pseudonim, "\n")] = 0;
 
-            case 0:
-                printf("Zamykanie programu...\n");
-                break;
+            if (usun_postac(&lista, pseudonim))
+                printf("Postac usunieta.\n");
+            else
+                printf("Nie znaleziono postaci.\n");
+            break;
+        }
 
-            default:
-                printf("Niepoprawny wybor.\n");
-                break;
+        case 5:
+            wyswietl_liste(&lista);
+            break;
+
+        case 0:
+            printf("Zamykanie programu...\n");
+            break;
+
+        default:
+            printf("Niepoprawny wybor.\n");
         }
 
     } while (wybor != 0);
@@ -38,3 +67,4 @@ int main(void) {
     zwolnij_liste(&lista);
     return 0;
 }
+
