3276d2009ff0fb2e48db5178f85f19f2be9989cb
Kmiezio <107400240+Kmiezio@users.noreply.github.com>
2026-01-03 19:52:22 +0100

    Update lista.h

--- lista.h
+++ lista.h
@@ -3,21 +3,21 @@
 
 #include "postac.h"
 
-
 typedef struct Wezel {
-    Postac dane;          
-    struct Wezel *next;   
+    Postac dane;
+    struct Wezel *next;
 } Wezel;
 
-
 typedef struct {
-    Wezel *head;          
+    Wezel *head;
 } Lista;
-
 
 void inicjalizuj_liste(Lista *lista);
 void dodaj_postac(Lista *lista, Postac p);
 void wyswietl_liste(const Lista *lista);
 void zwolnij_liste(Lista *lista);
+
+Postac* znajdz_postac(Lista *lista, const char *pseudonim);
+int usun_postac(Lista *lista, const char *pseudonim);
 
 #endif
