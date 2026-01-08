aa04affa67288dff920b244cd28da89ed751f25b
Kmiezio <107400240+Kmiezio@users.noreply.github.com>
2026-01-08 15:31:40 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 #include "menu.h"
 #include "lista.h"
 #include "postac.h"
+#include "pliki.h"
 
 int main(void) {
     Lista lista;
     inicjalizuj_liste(&lista);
+
     int wybor;
+    wczytaj_z_pliku(&lista, "baza.dat");
 
     do {
         wyswietl_menu();
@@ -49,10 +52,36 @@ int main(void) {
                 printf("Nie znaleziono postaci.\n");
             break;
         }
+        case 4: {
+    char pseudonim[MAX_TXT];
+    printf("Pseudonim do wyszukania: ");
+    fgets(pseudonim, MAX_TXT, stdin);
+    pseudonim[strcspn(pseudonim, "\n")] = 0;
+
+    Postac *p = znajdz_postac(&lista, pseudonim);
+    if (p) {
+        printf("\n--- ZNALEZIONA POSTAC ---\n");
+        printf("Pseudonim: %s\n", p->pseudonim);
+        printf("Rola: %d\n", p->rola);
+        printf("Moc: %s\n", p->moc);
+        printf("Poziom zagrozenia: %d\n", p->poziom_zagrozenia);
+        printf("Dzielnica: %s\n", p->dzielnica);
+        printf("Status: %d\n", p->status);
+    } else {
+        printf("Nie znaleziono postaci.\n");
+    }
+    break;
+}
+
 
         case 5:
             wyswietl_liste(&lista);
             break;
+
+            case 6:
+    sortuj_po_zagrozeniu(&lista);
+    printf("Posortowano po poziomie zagrozenia.\n");
+    break;
 
         case 0:
             printf("Zamykanie programu...\n");
@@ -64,7 +93,8 @@ int main(void) {
 
     } while (wybor != 0);
 
+    zapisz_do_pliku(&lista, "baza.dat");
+
     zwolnij_liste(&lista);
     return 0;
 }
-
