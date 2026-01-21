7bb1cf5085646acba0fff4803e65f967c453beee
Kmiezio <107400240+Kmiezio@users.noreply.github.com>
2026-01-21 15:19:08 +0100

    Update lista.h

--- lista.h
+++ lista.h
@@ -17,6 +17,11 @@ void dodaj_postac(Lista *lista, Postac p);
 void wyswietl_liste(const Lista *lista);
 void zwolnij_liste(Lista *lista);
 void sortuj_po_zagrozeniu(Lista *lista);
+void wyszukaj_po_pseudonimie(const Lista *lista, const char *prefiks);
+void wyszukaj_po_dzielnicy(const Lista *lista, const char *dzielnica);
+void wyszukaj_po_zagrozeniu(const Lista *lista, int poziom);
+void sortuj_po_pseudonimie(Lista *lista);
+
 
 
 Postac* znajdz_postac(Lista *lista, const char *pseudonim);
