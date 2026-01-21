a6fd5fa9ad792cee3119a17ebfbb887326edaf87
Kmiezio <107400240+Kmiezio@users.noreply.github.com>
2026-01-21 15:19:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,13 +5,23 @@
 #include "postac.h"
 #include "pliki.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    const char *plik;
+
+    if (argc < 2) {
+        printf("Uzycie: %s <plik_danych>\n", argv[0]);
+        return 1;
+    }
+
+    plik = argv[1];
+
     Lista lista;
     inicjalizuj_liste(&lista);
 
-    int wybor;
-    wczytaj_z_pliku(&lista, "baza.dat");
+    wczytaj_z_pliku(&lista, plik);
 
+
+    int wybor;
     do {
         wyswietl_menu();
         wybor = pobierz_wybor();
@@ -53,36 +63,59 @@ int main(void) {
             break;
         }
         case 4: {
-    char pseudonim[MAX_TXT];
-    printf("Pseudonim do wyszukania: ");
-    fgets(pseudonim, MAX_TXT, stdin);
-    pseudonim[strcspn(pseudonim, "\n")] = 0;
+    int opcja;
+    char tekst[MAX_TXT];
+    int liczba;
 
-    Postac *p = znajdz_postac(&lista, pseudonim);
-    if (p) {
-        printf("\n--- ZNALEZIONA POSTAC ---\n");
-        printf("Pseudonim: %s\n", p->pseudonim);
-        printf("Rola: %d\n", p->rola);
-        printf("Moc: %s\n", p->moc);
-        printf("Poziom zagrozenia: %d\n", p->poziom_zagrozenia);
-        printf("Dzielnica: %s\n", p->dzielnica);
-        printf("Status: %d\n", p->status);
-    } else {
-        printf("Nie znaleziono postaci.\n");
+    printf("1. Po pseudonimie (prefiks)\n");
+    printf("2. Po dzielnicy\n");
+    printf("3. Po poziomie zagrozenia >=\n");
+    printf("Wybor: ");
+    scanf("%d", &opcja);
+    getchar();
+
+    switch (opcja) {
+        case 1:
+            printf("Podaj prefiks pseudonimu: ");
+            fgets(tekst, MAX_TXT, stdin);
+            tekst[strcspn(tekst, "\n")] = 0;
+            wyszukaj_po_pseudonimie(&lista, tekst);
+            break;
+
+        case 2:
+            printf("Podaj dzielnice: ");
+            fgets(tekst, MAX_TXT, stdin);
+            tekst[strcspn(tekst, "\n")] = 0;
+            wyszukaj_po_dzielnicy(&lista, tekst);
+            break;
+
+        case 3:
+            printf("Podaj minimalny poziom zagrozenia: ");
+            scanf("%d", &liczba);
+            getchar();
+            wyszukaj_po_zagrozeniu(&lista, liczba);
+            break;
+
+        default:
+            printf("Niepoprawna opcja.\n");
     }
     break;
 }
+
 
 
         case 5:
             wyswietl_liste(&lista);
             break;
 
+
             case 6:
-    sortuj_po_zagrozeniu(&lista);
-    printf("Posortowano po poziomie zagrozenia.\n");
+    sortuj_po_pseudonimie(&lista);
+    printf("Posortowano alfabetycznie po pseudonimie.\n");
     break;
 
+
+    
         case 0:
             printf("Zamykanie programu...\n");
             break;
@@ -93,8 +126,7 @@ int main(void) {
 
     } while (wybor != 0);
 
-    zapisz_do_pliku(&lista, "baza.dat");
-
+     zapisz_do_pliku(&lista, plik);
     zwolnij_liste(&lista);
     return 0;
 }
